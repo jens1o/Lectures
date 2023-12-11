ab802c433c0b2c86ef066bb008e281cea0047a53
Michael Eichberg <mail@michael-eichberg.de>
2023-12-11 12:59:37 +0100

    fixed shebang

--- push-pages.zsh
+++ push-pages.zsh
@@ -1,4 +1,5 @@
-#!/usr/bin/zsh
+#!/bin/zsh
 
 rsync -R **/**(.html|.webp|.svg|.png|.mov|.jpg) ~/Sites/delors.github.io 
 rsync -R LectureDoc2/**/** ~/Sites/delors.github.io
+rsync -R reStructuredTextToLectureDoc2/**/**(.html|.webp|.svg|.png|.mov|.jpg) ~/Sites/delors.github.io
