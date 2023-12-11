08c5cfc3cb48a823c243d208565057764341032e
Michael Eichberg <mail@michael-eichberg.de>
2023-12-11 15:32:54 +0100

    fixes of the glob

--- push-pages.zsh
+++ push-pages.zsh
@@ -2,6 +2,6 @@
 
 setopt extendedglob
 
-rsync -v -R **/**(.html|.webp|.svg|.png|.mov|.jpg)~**/*.dgraph/**\|**/*.graffle/** ~/Sites/delors.github.io 
-rsync -v -R LectureDoc2/**/**~**/Icon\? ~/Sites/delors.github.io
+rsync -v -R **/**(.html|.webp|.svg|.png|.mov|.jpg)~**/*.dgraph/**~**/*.graffle/** ~/Sites/delors.github.io 
+rsync -v -R LectureDoc2/**/**~**/Icon?~**/*.rst ~/Sites/delors.github.io
 rsync -v -R reStructuredTextToLectureDoc2/**/**(.html|.webp|.svg|.png|.mov|.jpg) ~/Sites/delors.github.io
