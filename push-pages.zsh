0e646816a8dd273807475384a00b38fef5eb4e2e
Michael Eichberg <mail@michael-eichberg.de>
2024-02-12 14:13:08 +0100

    generated pdfs will now also be published

--- push-pages.zsh
+++ push-pages.zsh
@@ -2,8 +2,8 @@
 
 setopt extendedglob
 
-rsync -v -R **/**(.html|.webp|.svg|.png|.mov|.jpg)~**/*.dgraph/**~**/*.graffle/** ~/Sites/delors.github.io 
+rsync -v -R  **/**(.html|rst.html.pdf|.webp|.svg|.png|.mov|.jpg)~**/*.dgraph/**~**/*.graffle/** ~/Sites/delors.github.io 
 rsync -v -R LectureDoc2/**/**~**/Icon?~**/*.rst ~/Sites/delors.github.io
-rsync -v -R reStructuredTextToLectureDoc2/**/**(.html|.webp|.svg|.png|.mov|.jpg) ~/Sites/delors.github.io
+rsync -v -R reStructuredTextToLectureDoc2/**/**(.html|.rst.html.pdf|.webp|.svg|.png|.mov|.jpg) ~/Sites/delors.github.io
 
 rst2pdf LectureDoc2/readme.rst ~/Sites/delors.github.io/LectureDoc2/readme.pdf  
