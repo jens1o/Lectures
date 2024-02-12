8c43bfd74ecc877dd1adae5d9cafae03144ed560
Michael Eichberg <mail@michael-eichberg.de>
2024-02-12 14:13:54 +0100

    added scripts to generate pdfs from HTML slides

---
+++ gen-pdfs-from-slides.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh 
+
+set -e
+
+function update_pdf_if_necessary() {
+    pdf_file="$1.pdf"   
+    if [[ ! -f "$pdf_file" || "$pdf_file" -ot "$1" ]]
+    then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') updating:" $pdf_file 
+        osascript gen-pdfs-from-slides.scpt "$1" 
+    fi
+}
+
+function check_all_rst_html_files() {
+    for f in **/*.rst.html 
+    do
+        update_pdf_if_necessary "$PWD/$f"
+    done
+}
+
+# Checks for each all ".rst.html" file if the corresponding pdf file
+# needs to be updated.
+check_all_rst_html_files
