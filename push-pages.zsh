1cdd9a5a879cc576c182a8a9a7a33cc01360b1ad
Michael Eichberg <mail@michael-eichberg.de>
2024-04-17 12:05:40 +0200

    ignoring graffles

--- push-pages.zsh
+++ push-pages.zsh
@@ -3,7 +3,7 @@
 setopt extendedglob
 
 rsync -v -R  **/**(.html|.rst.html.pdf|.webp|.svg|.png|.mov|.jpg|.ipynb)~**/*.dgraph/**~**/*.graffle/**~**/external/**~**/private/** ~/Sites/delors.github.io 
-rsync -v -R LectureDoc2/**/**~**/Icon?~**/*.rst ~/Sites/delors.github.io
+rsync -v -R LectureDoc2/**/**~**/Icon?~**/*.rst~**/*.graffle/** ~/Sites/delors.github.io
 rsync -v -R reStructuredTextToLectureDoc2/**/**(.html|.rst.html.pdf|.webp|.svg|.png|.mov|.jpg) ~/Sites/delors.github.io
 
 rst2pdf LectureDoc2/readme.rst ~/Sites/delors.github.io/LectureDoc2/readme.pdf  
