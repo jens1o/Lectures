e8f291bcd39998d0eae2ffeb9381f103ff6acb75
Michael Eichberg <mail@michael-eichberg.de>
2024-05-16 09:57:08 +0200

    we no longer generate a PDF from the readme.rst directly - we are now using LectureDoc

--- push-pages.zsh
+++ push-pages.zsh
@@ -3,7 +3,5 @@
 setopt extendedglob
 
 rsync -v -R  **/**(.html|.rst.html.pdf|.webp|.svg|.png|.mov|.jpg|.ipynb)~**/*.dgraph/**~**/*.graffle/**~**/external/**~**/private/** ~/Sites/delors.github.io 
-rsync -v -R LectureDoc2/**/**~**/Icon?~**/*.rst~**/*.graffle/** ~/Sites/delors.github.io
+rsync -v -R LectureDoc2/**/**~**/Icon?~**/*.rst~**/*.graffle/**~LectureDoc2/jsdoc/**~LectureDoc2/node_modules/**~LectureDoc2/package*.json ~/Sites/delors.github.io
 rsync -v -R reStructuredTextToLectureDoc2/**/**(.html|.rst.html.pdf|.webp|.svg|.png|.mov|.jpg) ~/Sites/delors.github.io
-
-rst2pdf LectureDoc2/readme.rst ~/Sites/delors.github.io/LectureDoc2/readme.pdf  
