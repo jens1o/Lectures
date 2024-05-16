99cd5dae6aa9115d21e31100401582a7936942f5
Michael Eichberg <mail@michael-eichberg.de>
2024-05-16 10:58:16 +0200

    preliminary code

---
+++ ds-xml/code/README.md
@@ -0,0 +1,19 @@
+# Voraussetzungen
+
+Installieren Sie die neueste Version von node.js von https://nodejs.org/en. Benutzen Sie bitte _die neueste Version_ auch wenn diese keine LTS Version ist.
+
+
+
+## Installiere benötige node.js Pakete
+
+Am Besten einfach im "aktuellen Verzeichnis" in dem die Übungsdateien liegen ausführen:
+
+### https://www.npmjs.com/package/xpath
+
+  npm install xpath
+
+### https://www.npmjs.com/package/xmldom
+
+  npm install @xmldom/xmldom  
+
+
---
+++ ds-xml/code/xpaths_evaluator.js
@@ -0,0 +1,84 @@
+/*
+ * Evaluates a set of XPath expressions against an XML file.
+ *
+ * Usage: node xpaths_evaluator.js <spec.json>
+ * 
+ * The specification document is a JSON file with the specified JSON schema (see `specSchema` below).
+ *
+ * @author Michael Eichberg
+ */
+
+const specSchema = {
+    "title": "XPath Evaluator Specification",
+    "description": "A specification for evaluating XPath expressions against an XML file",
+    "type": "object",
+    "properties": {
+        "source": {
+            "description": "The path to the XML file.",
+            "type": "string",
+        },
+        "namespaces": {
+            "description": "Namespaces used in the XPath expressions; keys are the prefixes, values are the URIs; i.e., the names.",
+            "type": "object",
+        },
+        "xpaths": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "expr": "string",
+                    "subExpr": "string"
+                },
+                "additionalProperties": false,
+                "required": ["expr"]
+            }
+        }
+    },
+    "required": ["source"]
+}
+
+const fs = require("fs");
+const xpath = require('xpath');
+const dom = require('@xmldom/xmldom').DOMParser;
+
+
+if (process.argv.length < 3) {
+    console.error("Usage: node xpaths_evaluator.js <spec.json>");
+    process.exit(1);
+}
+
+var spec = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+
+const xmlFile = fs.readFileSync(spec.source, "utf8");
+const xmlDOM = new dom().parseFromString(xmlFile);
+const namespaces = spec.namespaces ? spec.namespaces : {};
+
+spec.xpaths.forEach(xpathSpec => {
+    const baseExpr = xpathSpec["expr"];
+    const subExpr = xpathSpec["subExpr"];
+
+    console.log(`Evaluating "${baseExpr}":`);
+    xpath.
+        parse(baseExpr).
+        select({ node: xmlDOM, namespaces: namespaces }).
+        forEach((element, i) => {
+            if (subExpr) {
+                console.log(`${i}: Evaluating "${subExpr}" for node "${element.toString()}":\n`);
+                let result = xpath.evaluate(
+                    subExpr,
+                    element,
+                    (prefix) => namespaces[prefix],
+                    xpath.XPathResult.ANY_TYPE,
+                    null);
+                let node = result.iterateNext()
+                while (node) {
+                    console.log(node.toString());
+                    node = result.iterateNext();
+                }
+            } else {
+                console.log(`${i}: ${element.toString()}\n`);
+            }
+        });
+    console.log(`\n`);
+});
+
